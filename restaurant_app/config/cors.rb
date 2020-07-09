2b685e24d5b1d3bf4188f54062a63e2040f2629b
Mario90 <mgpinan1990@gmail.com>
2020-07-09 10:53:08 +0100

    Set up cors, set up session store and user can be created

---
+++ restaurant_app/config/cors.rb
@@ -0,0 +1,15 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+
+    allow do
+        origins "http://localhost/3000"
+        resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head],
+        credentials: true
+    end
+
+    allow do
+        origins "https://point-of-sale.herokuapp.com"
+        resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head],
+        credentials: true
+    end
+
+end
