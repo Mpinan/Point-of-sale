ef3fe69755809c85e41dfcac9ce8d71db7610311
Mario90 <mgpinan1990@gmail.com>
2020-07-10 09:56:03 +0100

    Added current user concern

---
+++ restaurant_app/app/controllers/signup_controller.rb
@@ -0,0 +1,24 @@
+class SignupController < ApplicationController
+    
+    def create
+        user = User.create!(
+            email: params["user"]["email"],
+            password: params["user"]["password"],
+            password_confirmation: params["user"]["password_confirmation"],
+            pin: params["user"]["pin"]
+        )
+        
+        if user
+            session[:user_id] = user.user_id
+            render json: {
+                status: :created,
+                user: user
+            }
+        else
+            render json: {
+                status: 500
+            }
+        end
+
+    end
+end
